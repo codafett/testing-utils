82406833c03a602ab997e9bdb9edfb2b04a5c476
Reuben Greaves <64164557+codafett@users.noreply.github.com>
2023-04-18 14:58:48 +0100

    Renam method and export (#10)
    Co-authored-by: tiney <developers@tiney.co>

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './db-utils';
+export * from './mock-async-call';
 export * from './mock-http-calls';
 export * from './server-utils';
---
+++ src/utils/mock-async-call.ts
@@ -0,0 +1,5 @@
+export function mockAsyncCall(instance: any, functionName: string, mockImplementation: () => Promise<any> = () => Promise.resolve('done')) {
+  return jest
+    .spyOn(instance, functionName)
+    .mockImplementation(mockImplementation);
+}
